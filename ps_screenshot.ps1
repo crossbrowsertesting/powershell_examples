b8d687e15c9898c22b6b4acc89506f497f9c6848
Harold Schreckengost <harold@hmschreck.local>
2017-10-26 14:32:14 -0500

    Commented ps_screenshot tool

--- ps_screenshot.ps1
+++ ps_screenshot.ps1
@@ -1,23 +1,30 @@
 Add-Type -AssemblyName System.Web
 
-
+# Specify your username and authKey
 $username = "you@yourDomain.com"
 $authKey = "yourActualAuthKey"
+
+# This is used for doing the HTTP basic authentication we use.  This converts the strings into a HTTP header.
 $Headers = @{ Authorization = "Basic {0}" -f [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$authKey))) }
 
 $apiEndpoint = "crossbrowsertesting.com/api/v3/screenshots"
 
+#Generates the string of browsers used for the Test
+# Sets to ?, then appends browsers in OS|Browser format
 $browserString = "?"
 Import-CSV browsers.csv | ForEach-Object {
     $browserString += "browsers=$($_.os)|$($_.browser)$($_.version)&"
 }
 
+# Iterate over the list of URLs and run a screenshot test for each one
 Get-Content urlList.txt | ForEach-Object {
     $url = $_
     $requestUrl = "https://$($apiEndpoint)/$($browserString)send_email=false"
     Write-Host "Sending screenshot request for $($url)"
     $params = @{url="$($url)"}
     $reqResponse = Invoke-WebRequest -Uri $requestUrl -Method POST -Headers $Headers -Body $params | ConvertFrom-JSON
+    
+    # Prevent the next test from starting until 30 seconds after this one is done.
     $session = $reqResponse.screenshot_test_id
     $version = $reqResponse.versions.version_id
     $running = "True"
